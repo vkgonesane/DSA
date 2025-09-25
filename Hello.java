b33c6076e4680736f41acc99ed05ba27226f5739
vkgonesane <vaibhav.vkv2004@gmail.com>
2025-09-25 23:19:37 +0530

    Hello World Code

---
+++ Hello.java
@@ -0,0 +1,8 @@
+import java.util.*; // Importing the entire utility package (not required here, but included as requested)
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
