851ffe4fcd56b15a11d1cd3fce0ef9866284c056
vkgonesane <vaibhav.vkv2004@gmail.com>
2025-09-28 10:51:40 +0530

    made changes

--- GFG/SecLargest.java
+++ GFG/SecLargest.java
@@ -1,4 +1,22 @@
 package GFG;
 
-public class SecLargest {
+class SecLargest {
+    public static void main(String[] args) {
+        int arr[] = {12, 35, 1, 10, 34, 35};
+
+        int largest = arr[0];
+        int second = -1;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            } else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+        }
+
+        System.out.println("Largest: " + largest);
+        System.out.println("Second Largest: " + second);
+    }
 }
