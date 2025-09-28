73fb25637bf0239854432b4f8a619794ca63db39
vkgonesane <vaibhav.vkv2004@gmail.com>
2025-09-28 13:56:46 +0530

    made changes

--- GFG/LCP.java
+++ GFG/LCP.java
@@ -1,4 +1,28 @@
 package GFG;
 
-public class LCP {
+import java.util.*;
+
+class LCP {
+    public static void main(String[] args){
+        String str[] = {"flower", "flow", "flight"};
+
+        if(str == null || str.length == 0){
+            System.out.println("LCP:");
+            return;
+        }
+
+        String prefix = str[0];
+
+        for(int i = 1; i < str.length; i++){   
+            while(str[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if(prefix.isEmpty()){
+                    System.out.println("LCP:");
+                    return;
+                }
+            }
+        }
+
+        System.out.println("LCP: " + prefix);
+    }
 }
